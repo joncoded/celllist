241f4b4a38ee38c4a496b5f844e92e3a7e7ab56e
joncoded <joncoded@gmail.com>
2021-02-09 23:55:49 -0500

    making tables responsive

--- src/components/CellList/CellList.js
+++ src/components/CellList/CellList.js
@@ -8,7 +8,7 @@ const CellList = (props) => {
     if (!sortSetting) {
       return;
     }
-    return sortSetting.field === field ? sortSetting.order : undefined;
+    return sortSetting.field === field ? sortSetting.order : '';
   }
 
   return (
